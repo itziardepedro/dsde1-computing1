84b9523b87a94a20ebaac5b0acdff5ccf0c61cb7
Itziar <id619@ic.ac.uk>
2019-10-17 14:48:28 +0100

    version1

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -1,54 +1,27 @@
-'''
-toys.py
-
-Simple toy functions to get comfortable working 
-with functions.
-poop
-'''
+import sys
 
 
-# write a function that adds 1
-# to the input and prints the result
-def inc(a):
+
+sys.exit()
+
+#question 1
+def addOne(a):
+    a += 1
     print(a)
+addOne(int(input('enter a number')))
 
+#question 2
+def add1(a):
+    a += 1
+    return a
+a = int(input('enter a number'))
+print(add1(a))
 
-# write a function that adds 1
-# to the input and returns the result
-def inc_return(a):
-    return # hint this is incomplete
+#question 3 
+def sum(a, b): 
+    return a+b
 
-
-# write a function that adds
-# the two input numbers together
-# and returns the sum
-def sum(a, b):
-    return
-
-
-# write a function that takes two
-# numbers, adds them together using 
-# sum() and then increments the sum
-# using inc_return
-def sum_inc(a, b):
-    return
-
-
-# write a function that returns a 
-# boolean (True or False) for whether 
-# the input number is even
-def is_even(a):
-    return
-
-
-# create for loop that takes a string
-# and an integer and returns a new string 
-# that is the string repeated equal to 
-# integer
-# e.g. string_repeat('ho', 3) returns
-# 'hohoho'
-def string_repeat(phrase, repeat):
-    # hint: you can add strings together 
-    # in order to concatenate them
-    return
+a = int(input('first number'))
+b = int(input('second number'))
+sum()  
 
