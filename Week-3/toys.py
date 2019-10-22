553f726fbe96929b17722ff8d193341b64a11859
Itziar <id619@ic.ac.uk>
2019-10-22 11:20:31 +0100

    trash

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -1,27 +1,57 @@
-import sys
-
-
-
-sys.exit()
-
 #question 1
+# write a function that adds 1
+# to the input and prints the result
 def addOne(a):
     a += 1
     print(a)
-addOne(int(input('enter a number')))
 
 #question 2
+# write a function that adds 1
+# to the input and returns the result
 def add1(a):
     a += 1
     return a
-a = int(input('enter a number'))
-print(add1(a))
 
 #question 3 
+# write a function that adds
+# the two input numbers together
+# and returns the sum
 def sum(a, b): 
     return a+b
+ 
+#question 4
+# write a function that takes two
+# numbers, adds them together using 
+# sum() and then increments the sum
+# using inc_return
+def sum_inc(a,b):
+    return add1(sum(a,b))
 
-a = int(input('first number'))
-b = int(input('second number'))
-sum()  
+#question 5
+# write a function that returns a 
+# boolean (True or False) for whether 
+# the input number is even
+def is_even(a):
+    a = input('choose a number')
+    even = False 
+    if a % 2: 
+        even = True 
+        return even 
+    else: 
+        return even
 
+#question 6 
+# create for loop that takes a string
+# and an integer and returns a new string 
+# that is the string repeated equal to 
+# integer
+# e.g. string_repeat('ho', 3) returns
+# 'hohoho'
+def string_repeat(phrase, repeat):
+    for i in range(repeat): 
+        print('ho')
+
+    
+    # hint: you can add strings together 
+    # in order to concatenate them
+    return
---
+++ prueba.py
@@ -0,0 +1,7 @@
+print ('What time is it?')
+
+time = input() 
+if int(time) >= 10: 
+    print('Its late')
+else: 
+    print('okay')
